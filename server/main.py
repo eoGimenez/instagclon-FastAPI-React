69410971da858ca3e9158fbc091433a5b026e96f
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-08-03 11:50:41 +0200

    limpiando para deploy de testeo

--- server/main.py
+++ server/main.py
@@ -1,9 +1,7 @@
-from fastapi import FastAPI, Depends
-from typing_extensions import Annotated
+from fastapi import FastAPI
 from routers import auth, post, comment
 from models.models import Base
 from config.database import engine
-from enviroment.config import Settings, get_settings
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -12,9 +10,6 @@ origins = [
     'http://localhost:3000'
 ]
 
-@app.get('/')
-def root(settings: Annotated[Settings, Depends(get_settings)]):
-    return settings
 
 app.include_router(auth.router)
 app.include_router(post.router)
