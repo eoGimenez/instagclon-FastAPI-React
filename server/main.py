d85519f64de0a1fbd17797ed272be9d35f04e9c9
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-08-04 00:43:42 +0200

    CORS test

--- server/main.py
+++ server/main.py
@@ -3,12 +3,39 @@ from routers import auth, post, comment
 from models.models import Base
 from config.database import engine
 from fastapi.middleware.cors import CORSMiddleware
-from enviroment.config import Settings, get_settings
+from fastapi.middleware import Middleware
+# from starlette.middleware.cors import CORSMiddleware
+# from enviroment.config import Settings, get_settings
 
-dotenv : Settings = get_settings()
+# dotenv : Settings = get_settings()
 
-app = FastAPI()
+originis = [
+    "http://localhost:3000",
+    "localhost:3000",
+    "http://localhost",
+    "https://localhost:3000"
+]
 
+middleware = [
+    Middleware(
+        CORSMiddleware,
+    allow_origins= originis,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )
+]
+
+app = FastAPI(middleware=middleware)
+print(originis)
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins= originis,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(auth.router)
 app.include_router(post.router)
@@ -16,11 +43,3 @@ app.include_router(comment.router)
 
 
 Base.metadata.create_all(engine)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins= [dotenv.ORIGIN],
-    allow_credentials = True,
-    allow_methods = ['*'],
-    allow_headers = ['*'],
-)
