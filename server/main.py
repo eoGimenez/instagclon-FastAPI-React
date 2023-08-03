f5043b0fd864c05756b27757d72230e8514eaeee
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-08-04 00:10:42 +0200

    testeando CORS del deploy

--- server/main.py
+++ server/main.py
@@ -3,24 +3,24 @@ from routers import auth, post, comment
 from models.models import Base
 from config.database import engine
 from fastapi.middleware.cors import CORSMiddleware
+from enviroment.config import Settings, get_settings
+
+dotenv : Settings = get_settings()
 
 app = FastAPI()
-
-origins = [
-    'http://localhost:3000'
-]
 
 
 app.include_router(auth.router)
 app.include_router(post.router)
 app.include_router(comment.router)
 
+
+Base.metadata.create_all(engine)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= origins,
+    allow_origins= [dotenv.ORIGIN],
     allow_credentials = True,
     allow_methods = ['*'],
     allow_headers = ['*'],
 )
-
-Base.metadata.create_all(engine)
