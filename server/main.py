a5e09ad6663fb0537b845085264164f1b8b4d522
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-08-04 19:36:37 +0200

    CORS

--- server/main.py
+++ server/main.py
@@ -3,39 +3,26 @@ from routers import auth, post, comment
 from models.models import Base
 from config.database import engine
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware import Middleware
-# from starlette.middleware.cors import CORSMiddleware
-# from enviroment.config import Settings, get_settings
 
-# dotenv : Settings = get_settings()
 
 originis = [
     "http://localhost:3000",
-    "localhost:3000",
     "http://localhost",
-    "https://localhost:3000"
+    "https://localhost:3000",
+    "http://0.0.0.0:3000"
 ]
 
-middleware = [
-    Middleware(
-        CORSMiddleware,
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
     allow_origins= originis,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=['GET', 'POST', 'PUT', 'DELETE'],
     allow_headers=["*"],
-    )
-]
-
-app = FastAPI(middleware=middleware)
-print(originis)
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins= originis,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+    expose_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(post.router)
