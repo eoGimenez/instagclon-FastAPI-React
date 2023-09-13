bf4a0312190b33f1f5ca78b9fa61361c3f5ed898
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-09-13 22:48:47 +0200

    integrando paginacion en la peticion de posts

--- server/config/db_post.py
+++ server/config/db_post.py
@@ -4,33 +4,41 @@ from models.models import DbPost
 from fastapi import HTTPException, status
 from datetime import datetime
 
+
 def post_post(db: Session, request: PostBase):
     new_post = DbPost(
-        image_url = request.image_url,
-        caption = request.caption,
-        timestamp = datetime.now(),
-        user_id = request.author_id 
-     )
+        image_url=request.image_url,
+        caption=request.caption,
+        timestamp=datetime.now(),
+        user_id=request.author_id
+    )
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
     return new_post
 
-def get_all_post(db: Session):
-    return db.query(DbPost).all()
 
-def get_post_by_id(db: Session, id:int):
+def get_all_post(db: Session, limit: int):
+    posts = db.query(DbPost).all()
+    return posts[-limit:]
+
+
+def get_post_by_id(db: Session, id: int):
     return db.query(DbPost).filter(DbPost.id == id).first()
+
 
 def delete_post(db: Session, id: int, user_id: int):
     post = db.query(DbPost).filter(DbPost.id == id).first()
     if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'No pudimos encontrar el post con id: {id}, intentelo nuevamente. ')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'No pudimos encontrar el post con id: {id}, intentelo nuevamente. ')
     if post.user_id != user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Solo puedes borrar post de los que seas propietario.')
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='Solo puedes borrar post de los que seas propietario.')
     db.delete(post)
     db.commit()
     return 'El post fue eliminado correctamente.'
+
 
 def get_all_user_posts(db: Session, user_id: int):
     posts = []
--- server/routers/post.py
+++ server/routers/post.py
@@ -14,22 +14,27 @@ router = APIRouter(
     tags=['Posts']
 )
 
+
 @router.post('/', response_model=PostDisplay)
 async def create_post(request: PostBase, db: Session = Depends(get_db), token: TokenData = Security(get_current_active_user, scopes=['post'])):
     return db_post.post_post(db, request)
 
 
 @router.get('/', response_model=List[PostDisplay])
-async def get_posts(db: Session = Depends(get_db)):
-    return db_post.get_all_post(db)
+async def get_posts(limit: int, db: Session = Depends(get_db)):
+    return db_post.get_all_post(db, limit)
+
 
 @router.post('/upload')
 async def upload_file(image: UploadFile = File(...), token: TokenData = Security(get_current_active_user, scopes=['post'])):
-    upload_result = cloudinary.uploader.upload(image.file, transformation = {"width": 800, "height": 600})
+    upload_result = cloudinary.uploader.upload(
+        image.file, transformation={"width": 800, "height": 600})
     return {
         'public_id': upload_result['public_id'],
         'url': upload_result['secure_url']
     }
+
+
 @router.get('/user_posts/{user_id}')
 async def users_posts(user_id: int, db: Session = Depends(get_db)):
     return db_post.get_all_user_posts(db, user_id)
@@ -38,6 +43,7 @@ async def users_posts(user_id: int, db: Session = Depends(get_db)):
 @router.get('/{id}', response_model=PostDisplay)
 async def get_post(id: int, db: Session = Depends(get_db)):
     return db_post.get_post_by_id(db, id)
+
 
 @router.delete('/{id}')
 async def delete_post(id: int, db: Session = Depends(get_db), token: TokenData = Security(get_current_active_user, scopes=['post'])):
