94b28ad1d1dfb4d6e0c8c6371680cd94a897347c
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-09-18 19:29:35 +0200

    creando endpoint PUT comment

--- server/routers/comment.py
+++ server/routers/comment.py
@@ -21,7 +21,7 @@ async def add_comment(resquest: CommentBase, db: Session = Depends(get_db), toke
     - **username**: Nombre del creador del comentario
     - **text**: Texto del comentario
     - **post_id**: ID unico de post a comentar
-"author_id": 0
+    - **author_id**: ID unico del autor del comentario
     
     """
     db_comment.post_comment(db, resquest)
@@ -33,8 +33,14 @@ async def add_comment(resquest: CommentBase, db: Session = Depends(get_db), toke
     return lala
 
 
-@router.get('/{post_id}', response_model=List[CommentDisplay])
+@router.get('/{post_id}', response_model=List[CommentDisplay], summary="Devuelve todos los comentario de un post")
 async def get_comments(post_id: int, db: Session = Depends(get_db)):
+    """
+    Devuelve un arreglo con todos los comentarios del post buscado por su ID
+
+    - **post_id**: ID unico del post a buscar en la base de datos
+
+    """
     lala = db_comment.get_all_comments(db, post_id)
     for resp in lala:
         if resp.responses:
@@ -42,10 +48,25 @@ async def get_comments(post_id: int, db: Session = Depends(get_db)):
                 pass
     return lala
 
-@router.get('/onecomment/{comment_id}', response_model=CommentDisplay)
+@router.get('/onecomment/{comment_id}', response_model=CommentDisplay, summary="Devuelve un comentario")
 async def get_one_comment(comment_id: int, db:Session = Depends(get_db)):
+    """
+    Devuelve un comentario por su ID unico
+
+    - **comment_id**: ID del comentario a buscar en la base de datos
+    
+    """
     return db_comment.get_comment_by_id(db, comment_id)
 
-@router.delete('/{comment_id}')
+@router.delete('/{comment_id}', summary="Elimina un comentario por su ID")
 async def delete_comment(comment_id: int,  db:Session= Depends(get_db), token: TokenData = Security(get_current_active_user, scopes=['post'])):
+    """
+    **¡¡REQUIERE ESTAR AUTENTICADO Y SER EL AUTOR DEL COMENTARIO!!**
+
+    Elimina el comentario por ID
+
+
+    - **comment_id**: ID del comentario a borrar de la base de datos
+
+    """
     return db_comment.delete_selected_comment(db, comment_id, token.id)
